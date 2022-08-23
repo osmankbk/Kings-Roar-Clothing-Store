d3f9306b39ea41b962cde1bd8d8b87817e6881e2
osmankbk <mackiekbk@yahoo.com>
2022-08-22 21:23:24 -0400

    Removes duplicate css styles and adds proper spacing between lines of code

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,14 +5,14 @@ import { CartContext } from '../../contexts/cartContext';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
-
+  const { isCartOpen, setIsCartOpen, cartItemCount } = useContext(CartContext);
+  
   const toggleCartDropDown = () => setIsCartOpen(!isCartOpen);
 
   return(
     <div onClick={ toggleCartDropDown } className='cart-icon-container'>
       <ShoppingBagIcon className='shopping-icon'/>
-      <span className='item-count'>10</span>
+      <span className='item-count'>{ cartItemCount} </span>
     </div>
   )
 }
