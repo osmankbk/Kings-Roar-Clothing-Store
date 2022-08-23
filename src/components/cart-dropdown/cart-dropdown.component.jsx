6b24845a6bb1d60efe018d86e307fd57b3b8da93
osmankbk <mackiekbk@yahoo.com>
2022-08-22 21:20:34 -0400

    Removes duplicate css styles

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,20 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cartContext';
 
 import Button from '../button/button.component';
-import './cart-dropdown.styles.scss';
-
 import CartItem from '../cart-item/cart-item.component';
+
+import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckOut = () => {
+    navigate('/checkout');
+  }
+
   return(
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -15,7 +22,7 @@ const CartDropdown = () => {
           cartItems.map((item) => <CartItem key={item.id} cartItem={item}/>)
         }
       </div>
-      <Button>CHECKOUT</Button>
+      <Button onClick={ goToCheckOut }>CHECKOUT</Button>
     </div>
   )
 }
